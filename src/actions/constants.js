55cf038e003b1605fe42be178dd4b139164ebde2
albertogiovanelli <alberto.giovanelli@i-seed.it>
2018-06-06 22:09:15 +0200

    semplified arrows and add constant

--- src/actions/constants.js
+++ src/actions/constants.js
@@ -1,3 +1,6 @@
 /**
  * Created by albertogiovanelli on 06/06/18.
  */
+export const ADD_TODO = 'ADD_TODO';
+export const TOGGLE_TODO = 'TOGGLE_TODO';
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
--- src/actions/todoActions.js
+++ src/actions/todoActions.js
@@ -1,26 +1,21 @@
 /**
  * Created by albertogiovanelli on 06/06/18.
  */
+import {ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER} from './constants';
 
 let nextTodoId = 0;
-export const addTodo = (text) => {
-    return {
-        type: 'ADD_TODO',
-        id: nextTodoId++,
-        text
-    }
-};
+export const addTodo = (text) => ({
+    type: ADD_TODO,
+    id: nextTodoId++,
+    text
+});
 
-export const toggleTodo = (id) => {
-    return {
-        type: 'TOGGLE_TODO',
-        id
-    }
-};
+export const toggleTodo = (id) => ({
+    type: TOGGLE_TODO,
+    id
+});
 
-export const setVisibilityFilter = (filter) => {
-    return {
-        type: 'SET_VISIBILITY_FILTER',
-        filter
-    }
-};
+export const setVisibilityFilter = (filter) => ({
+    type: SET_VISIBILITY_FILTER,
+    filter
+});
